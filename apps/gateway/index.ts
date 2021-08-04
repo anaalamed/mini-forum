969a7e6aefa3498c210aec2f45d8ca7dc90e551b
Ana <ana@Anas-MacBook-Air.local>
2021-08-04 11:42:35 +0300

    no [::1]

--- apps/gateway/index.ts
+++ apps/gateway/index.ts
@@ -27,8 +27,8 @@ app.use(['/api/comments', '/api/posts'], async function checkAuth(req, res, next
     next();
 })
 app.use('/api/comments', createProxyMiddleware({ target: 'http://localhost:4002', changeOrigin: true }));
-app.use('/api/posts', createProxyMiddleware({ target: 'http://[::1]:4000', changeOrigin: true }));
-app.use(['/api/users', '/api/me', '/api/register', '/api/change-password', '/api/login', '/api/logout'], createProxyMiddleware({ target: 'https://[::1]:4000', changeOrigin: true }));
+app.use('/api/posts', createProxyMiddleware({ target: 'http://localhost:4001', changeOrigin: true }));
+app.use(['/api/users', '/api/me', '/api/register', '/api/change-password', '/api/login', '/api/logout'], createProxyMiddleware({ target: 'https://localhost:4000', changeOrigin: true }));
 
 
 app.use(express.static(path.resolve(__dirname, '../../ui/build')));
